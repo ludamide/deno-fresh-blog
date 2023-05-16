f68252271893a56d9ac79550d12ad2b04ffded2f
Ludvig Amide <ludvig.amide@outlook.es>
2023-05-16 20:18:29 +0200

    Put our first island

---
+++ islands/Button.tsx
@@ -0,0 +1,17 @@
+import { JSX } from "preact";
+import { useState } from "preact/hooks";
+import { IS_BROWSER } from "$fresh/runtime.ts";
+
+export default function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
+  const [like, setLike] = useState(false);
+
+  return (
+    <button
+      onClick={() => setLike(!like)}
+      {...props}
+      class="px-2 py-1 border(gray-100 2) hover:bg-gray-200"
+    >
+      {like ? "💖 Dejar que me guste" : "🖤 Me gusta"}
+    </button>
+  );
+}
--- routes/blog/[id].tsx
+++ routes/blog/[id].tsx
@@ -1,6 +1,7 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
-import { loadPost } from "../../utils/posts.ts";
 import { CSS } from "https://deno.land/x/gfm@0.2.3/mod.ts";
+import Button from "../../islands/Button.tsx";
+import { loadPost } from "../../utils/posts.ts";
 
 export const handler: Handlers = {
   async GET(request, context) {
@@ -14,8 +15,11 @@ export default function PagePost(props: PageProps) {
   const { post } = props?.data || {};
   return (
     <article class="p-4">
-      <h1 class="text-2xl font-bold">{post.title}</h1>
-      <time>{Intl.DateTimeFormat("es").format(post.date)}</time>
+      <header>
+        <h1 class="text-2xl font-bold">{post.title}</h1>
+        <time>{Intl.DateTimeFormat("es").format(post.date)}</time>
+      </header>
+      <Button />
       <style dangerouslySetInnerHTML={{ __html: CSS }} />
       <div
         class="markdown-body"
